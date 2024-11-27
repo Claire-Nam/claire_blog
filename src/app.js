45d24d6fe2cf0b00f32e08158a8bcd2fb80b8f3b
Claire <nbr1023@naver.com>
2024-11-27 16:36:21 +0900

    db연결 > /db/index.js 서버 실행 > app으로 파일 분리

--- src/app.js
+++ src/app.js
@@ -1,32 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import { Sequelize } from "sequelize";
-import UserModels from "./db/models/user-model.js"; // User 모델 임포트
+import "./db/index.js"; // db 연결 및 모델 설정 로드
 
 dotenv.config(); // 환경변수 로드
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-// DB 연결 설정
-const sequelize = new Sequelize(
-  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-);
-
-// DB 연결 확인
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("DB 연결 성공");
-  })
-  .catch(err => {
-    console.log("DB 연결 실패:", err);
-  });
-
-// Express 서버 설정
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+const port = process.env.PORT || 3001;
 
 // 서버 실행
 app.listen(port, () => {
--- src/db/index.js
+++ src/db/index.js
@@ -1,24 +1,25 @@
-// import dotenv from "dotenv";
-// import { Sequelize } from "sequelize";
-// import UserModels from "./models/user-model";
-// import PostModels from "./models/post-model";
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+import UserModels from "./models/user-model.js";
+import PostModels from "./models/post-model.js";
 
-// dotenv.config();
+dotenv.config();
 
-// // db 연결
-// const sequelize = new Sequelize(
-//   `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-// );
+// db 연결
+const sequelize = new Sequelize(
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+);
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("DB 연결 성공");
-//   })
-//   .catch(err => {
-//     console.log("DB 연결 실패: ", err);
-//   });
+// DB 연결 확인
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("DB 연결 성공");
+  })
+  .catch(err => {
+    console.log("DB 연결 실패:", err);
+  });
 
-// // 모델 간 관계 정의
-// PostModels.belongsTo(UserModels, { foreignKey: "author_id" });
-// UserModels.hasMany(PostModels, { foreignKey: "author_id" });
+// 모델 간 관계 정의
+PostModels.belongsTo(UserModels, { foreignKey: "author_id" });
+UserModels.hasMany(PostModels, { foreignKey: "author_id" });
