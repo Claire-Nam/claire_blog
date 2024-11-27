8e0358add1a417ee73899acbe437fc632faae6de
Claire-Nam <nbr1023@naver.com>
2024-11-27 09:18:10 +0900

    first source commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { app } from "./src/app.js";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`https://localhost:${PORT}에서 응답 대기중`);
+});
